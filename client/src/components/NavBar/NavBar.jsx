9fe06befad2ac0a94dafb93c420dfadef180880d
ivantrejoc <IVANJTREJOC@GMAIL.COM>
2023-08-31 18:47:06 -0400

    paginated done

--- client/src/components/NavBar/NavBar.jsx
+++ client/src/components/NavBar/NavBar.jsx
@@ -4,86 +4,47 @@ import SearchBar from "../SearchBar/SearchBar";
 import logoPokemon from "../../assets/pokemon-logo.png";
 
 const NavBar = () => {
-
   const location = useLocation();
-  return ( 
+  return (
     <div>
+      <nav class="bg-red-600 dark:bg-blue-400 fixed w-full z-20 top-0 left-0 border-b border-gray-200 dark:border-gray-600">
+        <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+          <img src={logoPokemon} class="h-20 ml-0 mr-10" alt="Pokemon logo" />
 
-    
-      {/* <div class= "bg-red-600 h-50 fixed top-0 left-0 w-full p-2 flex flex-row items-center justify-between border-blue">
-        <img src={logoPokemon} class="h-20 ml-1 mr-10" alt="logo" />
-        <ul class="flex flex-row mt-auto mb-auto ">
-          <li className="p-2 mr-5 ml-5 tracking-wider flex-none transition duration-200 hover:scale-110 mt-auto mb-auto">
-          {location.pathname !== "/home" && <Link className="text-m text-white text-2xl fontPoppins hover:fontPoppinsB font-lato font-bold"
-    to="/home">Home</Link>}
+          <div class="flex md:order-2 items-end">
+            <SearchBar />
+          </div>
+          <div
+            class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1"
+            id="navbar-sticky"
+          >
+            <ul class="flex flex-col p-4 md:p-0 mt-4   md:flex-row md:space-x-8 md:mt-0 md:border-0 dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+              <li>
+                {location.pathname !== "/home" && (
+                  <Link
+                    className="text-m text-white text-2xl  hover:font-bold font-lato"
+                    to="/home"
+                  >
+                    Home
+                  </Link>
+                )}
               </li>
-          <li className="p-2 mr-5 ml-5 tracking-wider flex-none transition duration-200 hover:scale-110 mt-auto mb-auto">
-          {location.pathname !== "/create" && <Link className="text-m text-white text-2xl fontPoppins hover:fontPoppinsB font-lato font-bold" to="/create">Create Pokemon</Link>}
-          </li>
-          <li class= "p-2 mr-5 ml-5 mt-auto mb-auto flex items-end">
-          <SearchBar/>
-          </li>
-        </ul> 
-        </div> */}
-
-
-
-
-
-
-        <nav class="bg-red-600 dark:bg-blue-400 fixed w-full z-20 top-0 left-0 border-b border-gray-200 dark:border-gray-600">
-  <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
- 
-      <img src={logoPokemon} class="h-20 ml-0 mr-10" alt="Pokemon logo"/>
-      
-
-  <div class="flex md:order-2 items-end">
-            <SearchBar/>      
-  </div>
-  <div class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-sticky">
-    <ul class="flex flex-col p-4 md:p-0 mt-4   md:flex-row md:space-x-8 md:mt-0 md:border-0 dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
-      <li>
-      {location.pathname !== "/home" && <Link className="text-m text-white text-2xl  hover:font-bold font-lato"
-    to="/home">Home</Link>}
-      </li>
-      <li>
-      {location.pathname !== "/create" && <Link className="text-m text-white text-2xl hover:font-bold font-lato" to="/create">Create Pokemon</Link>}
-      </li>
-      
-    </ul>
-  </div>
-  </div>
-</nav>
-
-
-
-
-
-
-
-
-</div>
-
-
-
-
-     
-
-
-
-    
-      
-   
-    
-
-
-
-
-
-
-    
+              <li>
+                {location.pathname !== "/create" && (
+                  <Link
+                    className="text-m text-white text-2xl hover:font-bold font-lato"
+                    to="/create"
+                  >
+                    Create Pokemon
+                  </Link>
+                )}
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </div>
   );
 };
-
 
 export default NavBar;
--- client/src/components/Pagination/Pagination.jsx
+++ client/src/components/Pagination/Pagination.jsx
@@ -1,64 +1,61 @@
 import React from "react";
 
-
-const Pagination = ({
-  cardsPerPage,
-  allPokemons,
-  pagination,
-  currentPage  
-}) => {
-
-  
+const Pagination = ({ cardsPerPage, allPokemons, pagination, currentPage }) => {
   const pageNumbers = [];
 
   for (let i = 1; i <= Math.ceil(allPokemons / cardsPerPage); i++) {
     pageNumbers.push(i);
   }
 
-  
-  
   return (
-    <div class="flex flex-row items-center -space-x-px h-10 mb-4 mt-2text-base">
+    <div class="flex flex-row items-center -space-x-px h-10 mb-8 mt-2text-base">
       <ul class="flex items-center -space-x-px h-10 text-base">
-     
-          <button
-            class="fle items-center justify-center px-4 h-10 ml-0 leading-tight text-gray-500 bg-white border border-gray-300 rounded-l-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
-            onClick={() => pagination(currentPage-1)} disabled={currentPage === 1}>
-         
-            <span class="sr-only">Previous</span>
-            <svg
-              class="w-3 h-3"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 6 10"
-            >
-              <path
-                stroke="currentColor"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="M5 1 1 5l4 4"
-              />
-            </svg>
-          </button>
-      
+        <button
+          class="fle items-center justify-center px-4 h-10 ml-0 leading-tight text-gray-500 bg-white border border-gray-300 rounded-l-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+          onClick={() => pagination(currentPage - 1)}
+          disabled={currentPage === 1}
+        >
+          <span class="sr-only">Previous</span>
+          <svg
+            class="w-3 h-3"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 6 10"
+          >
+            <path
+              stroke="currentColor"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M5 1 1 5l4 4"
+            />
+          </svg>
+        </button>
+
         {pageNumbers?.map((number) => (
-            <li key={number}>
-              <button
-                class="flex items-center justify-center px-4 h-10 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
-                key={number}
-                onClick={() => pagination(number)} disabled ={pageNumbers === number}
-              >
-                {number}
-              </button>
-            </li>
-          ))}
+          <li key={number}>
+            <button
+              class={`flex items-center justify-center px-4 h-10 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white
+               ${
+                 currentPage === number
+                   ? " text-gray-900 focus:bg-red-600 focus-within:bg-red-600 dark:focus:bg-red-700 dark:focus-within:bg-red-700"
+                   : ""
+               }`}
+              key={number}
+              onClick={() => pagination(number)}
+              // disabled={currentPage === number}
+            >
+              {number}
+            </button>
+          </li>
+        ))}
 
         <li>
           <button
             class="flex items-center justify-center px-4 h-10 leading-tight text-gray-500 bg-white border border-gray-300 rounded-r-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
-            onClick={ ()=> pagination(currentPage+1) } disabled={currentPage === pageNumbers.length}
+            onClick={() => pagination(currentPage + 1)}
+            disabled={currentPage === pageNumbers.length}
           >
             <span class="sr-only">Next</span>
             <svg
