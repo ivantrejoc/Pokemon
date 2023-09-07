3e0d5392004810ebcb3c767ea92ac376fdf2119a
ivantrejoc <IVANJTREJOC@GMAIL.COM>
2023-09-07 10:40:06 -0400

    pg

--- api/src/db.js
+++ api/src/db.js
@@ -1,4 +1,5 @@
 require("dotenv").config();
+require('pg'); //INDISPENSABLE INSTALAR PARA DEPLOYAR BDD
 const { Sequelize } = require("sequelize");
 const fs = require("fs");
 const path = require("path");
