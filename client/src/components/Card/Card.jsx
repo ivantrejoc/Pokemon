cf8f90f4ec03503d08136b4a67fc5144ee47077a
ivantrejoc <IVANJTREJOC@GMAIL.COM>
2023-09-07 12:09:03 -0400

    cambio card

--- client/src/components/Card/Card.jsx
+++ client/src/components/Card/Card.jsx
@@ -6,7 +6,7 @@ const Card = ({ id, name, sprites, types }) => {
       <div class="flex flex-col justify-center items-center w-full max-w-xs h-full bg-yellow-300 border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 hover:bg-yellow-400 duration-300">
         <img class="p-8 rounded-t-lg w-40 h-60 mb-2 hover" src={sprites} alt={name} />
 
-        <div class="px-5 pb-5 flex items-center justify-between">
+        <div class="px-5 pb-5 flex items-center">
           <h3 class="text-3xl font-lato font-bold tracking-tight text-blue-800 dark:text-white">
             {name}
           </h3>
